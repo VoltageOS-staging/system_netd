42a02727ade2d602f87b77ac503611454be2fc8b
Dmitrii <bankersenator@gmail.com>
2026-06-21 18:20:30 +0300

    tmp
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- server/Controllers.cpp
+++ server/Controllers.cpp
@@ -335,7 +335,7 @@ void Controllers::init() {
         // As such simply exit netd.  This may crash loop the system, but by failing
         // to bootup we will trigger rollback and thus this offers us protection against
         // a mainline update breaking things.
-        exit(2);
+        //exit(2);
     }
     gLog.info("Enabling bandwidth control: %" PRId64 "us", s.getTimeAndResetUs());
 
